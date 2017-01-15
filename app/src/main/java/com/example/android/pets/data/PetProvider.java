3099b9beaa92e41f9dd26ecb1be64a22d64dfe4d
Kemp <kempm@wit.edu>
2017-01-15 02:57:00 -0500

    Add support for deletion of database rows

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -220,10 +220,30 @@ public class PetProvider extends ContentProvider {
 
     /**
      * Delete the data at the given selection and selection arguments.
+     * Return number of rows deleted.
      */
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+
+        // Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Find what kind of uri we passed int
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+
+            case PET_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PetEntry.COLUMN_ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri))};
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     /**
